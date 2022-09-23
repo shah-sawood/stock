ab599d3a3524ecaaa09a8ea18d1711a7754491c8
shinwaris <zoology.fb@gmail.com>
2022-09-24 02:43:05 +0500

    ..

--- stock/settings.py
+++ stock/settings.py
@@ -20,10 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8swmje$2rdsahti3%69ypi3qr)&--0p$jc$-434uuqzg5tj3+l"
+SECRET_KEY = "haja8*fjiea;&f28478s984akgj;lj9q-8ru4q4ruf80guswmje$2rdsahti3%69ypi3qr)&--0p$jc$-434uuqzg5tj3+l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ["*"]
 
